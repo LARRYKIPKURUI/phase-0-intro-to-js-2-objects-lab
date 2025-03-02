d98309c7e2dbe8a36886e22aea2f276614c38694
LARRY KIPKURUI <larrykipkurui12@gmail.com>
2025-03-02 23:25:11 +0300

    Objects and their methods

--- index.js
+++ index.js
@@ -1,1 +1,97 @@
 // Write your solution in this file!
+
+const employee = {
+    name: "John mbugua",
+    streetAddress: "123 Main St"
+};
+
+// Function to update an employee object non-destructively
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    return {
+        ...employee,
+        [key]: value
+    };
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue() {
+    employee.name = "Sam"
+    employee.streetAddress = "12 Broadway"
+    return employee;
+}
+
+function deleteFromEmployeeByKey(employee, key) {
+    const newEmployee = { ...employee };
+    delete newEmployee[key];
+    return newEmployee;
+}
+
+function destructivelyDeleteFromEmployeeByKey() {
+    const newEmployee = { ...employee };
+    delete employee.name;
+    delete employee.streetAddress;
+    return employee;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
